4e289ad6789cc638aea85766a302e9a838b7b369
GreenSlider21 <greenslider21@gmail.com>
2025-02-25 14:00:52 -0600

    added millis demo

---
+++ colour time swap/sketch.js
@@ -0,0 +1,37 @@
+// let someTime = 2000;
+let waitTime = 2000;
+let lastSwitchedTime = 0;
+let isWhite = true;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  // console.log(millis());
+  // if (millis() < someTime){
+  //   background("white");
+  // }
+  // else{
+  //   background("black");
+  // }
+  swapStateIfNeeded();
+  showBackground();
+}  
+
+function swapStateIfNeeded() {
+  if(millis() > lastSwitchedTime + waitTime){
+    isWhite = !isWhite
+    lastSwitchedTime = millis();
+  }
+}
+
+function showBackground(){
+  if(isWhite){
+    background("white");
+  }
+  else{
+    background("black");
+  }
+}
