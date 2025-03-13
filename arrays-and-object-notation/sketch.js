ade5acc716f24466968a64dd92556ecca53c121d
GreenSlider21 <greenslider21@gmail.com>
2025-03-13 17:10:36 -0600

    gave worth to cards
    each card has an appropriate worth and i added a secondary worth for the aces so they can be 1 or 11. imported card images although I forgot how to use images.

--- arrays-and-object-notation/sketch.js
+++ arrays-and-object-notation/sketch.js
@@ -5,33 +5,40 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-
 let deck = [];
 let cardNum = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
-let suit = ["hearts ", "diamonds ", "spades ", "clubs "];
-
-for (let num = 0; num < cardNum.length; num++) {
-  for (let type = 0; type < suit.length; type++) {
-    let card = {
-      suit_value: suit[type],
-      number_value: cardNum[num],
-    };
-    deck.push(card);
-  }
-}
-
+let cardSuit = ["hearts", "diamonds", "spades", "clubs"];
+let cardWorth = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
+let secondaryWorth = [11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   createDeck();
-
 }
 
 function draw() {
   background(220);
+  // console.log(deck[0].suit_value);
+  // console.log(deck[0].number_value);
+  // console.log(deck[0].worth_value);
+  // console.log(deck[0].secondary_value);
 }
 
 
 function createDeck() {
+  for (let num = 0; num < cardNum.length; num++) {
+    for (let suit = 0; suit < cardSuit.length; suit++) {
+      let card = {
+        suit_value: cardSuit[suit],
+        number_value: cardNum[num],
+        worth_value: cardWorth[num],
+        secondary_value: secondaryWorth[num],
+      };
+      deck.push(card);
+    }
+  }
+}
 
+function draw_card() {
+  
 }
