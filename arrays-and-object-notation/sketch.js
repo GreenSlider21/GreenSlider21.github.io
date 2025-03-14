9fc08b1fc0a3697d4d2ad84c8c0b65d2bb064e76
GreenSlider21 <greenslider21@gmail.com>
2025-03-14 14:16:56 -0600

    tried to make an array with every card image in it

--- arrays-and-object-notation/sketch.js
+++ arrays-and-object-notation/sketch.js
@@ -5,11 +5,18 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let cardImages = [];
+let allImages = "deck_images/";
 let deck = [];
-let cardNum = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
-let cardSuit = ["hearts", "diamonds", "spades", "clubs"];
-let cardWorth = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
-let secondaryWorth = [11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let cardNum = [2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "J", "K", "Q"];
+let cardSuit = ["clubs", "diamonds", "hearts", "spades"];
+let cardWorth = [2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 10, 10, 10];
+
+function preload(){
+  for (let png = 0; png < allImages.length; png++) {
+    cardImages.push(loadImage("deck_images/" + allImages[png] + ".png"));
+  }
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -21,7 +28,7 @@ function draw() {
   // console.log(deck[0].suit_value);
   // console.log(deck[0].number_value);
   // console.log(deck[0].worth_value);
-  // console.log(deck[0].secondary_value);
+  image(cardImages, width/2, height/2);
 }
 
 
@@ -32,7 +39,6 @@ function createDeck() {
         suit_value: cardSuit[suit],
         number_value: cardNum[num],
         worth_value: cardWorth[num],
-        secondary_value: secondaryWorth[num],
       };
       deck.push(card);
     }
