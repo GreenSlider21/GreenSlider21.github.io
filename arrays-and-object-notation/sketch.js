564efbe0e758fa7976b0f37951646e05fa44e98e
GreenSlider21 <greenslider21@gmail.com>
2025-03-15 12:54:33 -0600

    Image array
    created an image array that preloads all the images as well as giving each card an appropriate value so that when that card is called it will look at what card image it should be displaying

--- arrays-and-object-notation/sketch.js
+++ arrays-and-object-notation/sketch.js
@@ -5,16 +5,22 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let cardImages = [];
-let allImages = "deck_images/";
+let clubsImages = [];
+let diamondsImages = [];
+let heartsImages = [];
+let spadesImages = [];
 let deck = [];
-let cardNum = [2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "J", "K", "Q"];
+let cardNum = [2, 3, 4, 5, 6, 7, 8, 9, 10, "ace", "jack", "king", "queen"];
 let cardSuit = ["clubs", "diamonds", "hearts", "spades"];
 let cardWorth = [2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 10, 10, 10];
+let cardImage = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 function preload(){
-  for (let png = 0; png < allImages.length; png++) {
-    cardImages.push(loadImage("deck_images/" + allImages[png] + ".png"));
+  for (let png = 0; png < cardNum.length; png++) {
+    clubsImages.push(loadImage("deck_images/" + cardNum[png] + "_of_clubs.png"));
+    diamondsImages.push(loadImage("deck_images/" + cardNum[png] + "_of_diamonds.png"));
+    heartsImages.push(loadImage("deck_images/" + cardNum[png] + "_of_hearts.png"));
+    spadesImages.push(loadImage("deck_images/" + cardNum[png] + "_of_spades.png"));
   }
 }
 
@@ -28,7 +34,7 @@ function draw() {
   // console.log(deck[0].suit_value);
   // console.log(deck[0].number_value);
   // console.log(deck[0].worth_value);
-  image(cardImages, width/2, height/2);
+  draw_card();
 }
 
 
@@ -39,6 +45,7 @@ function createDeck() {
         suit_value: cardSuit[suit],
         number_value: cardNum[num],
         worth_value: cardWorth[num],
+        image_value: cardImage[num],
       };
       deck.push(card);
     }
@@ -46,5 +53,7 @@ function createDeck() {
 }
 
 function draw_card() {
-  
+  if (deck[0].suit_value === "clubs"){
+    image(clubsImages[deck[0].image_value], width/2, height/2);
+  }  
 }
